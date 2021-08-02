babb4645938c9249c67ca289c57045b33208fb78
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2021-08-02 23:33:47 +0300

    added state in TodoListItem Component, onImportantClick listener

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,26 +2,43 @@ import React, {Component} from 'react'
 import './todo-list-item.css'
 
 export default class TodoListItem extends Component{
+  state = {
+    done: false,
+    important: false
+  }
   onLabelClick = ()=> {
-    console.log(this.props.label);
+    this.setState({
+      done: !this.state.done
+    })
+  }
+  onImportantClick = () => {
+    this.setState({
+      important: !this.state.important
+    })
   }
 
   render(){
-    const {label, important = false} = this.props;
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
+    const {label} = this.props;
+    const {done, important} = this.state;
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done'
     }
+    if (important) {
+      classNames += ' important'
+    }
+    
     return (
-      <span className='todo-list-item'>
+      <span className={classNames}>
       <span
         className='todo-list-item-label'
-        style={style}
+        /* style={style} */
         onClick = {this.onLabelClick}>
           {label}
       </span>
         <button type='button'
-                className='btn btn-outline-success btn-sm float-right'>
+                className='btn btn-outline-success btn-sm float-right'
+                onClick = {this.onImportantClick}>
           <i className='fa fa-exclamation'></i>
         </button>
         <button type='button'
